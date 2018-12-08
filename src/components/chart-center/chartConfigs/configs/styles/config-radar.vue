5b8a650137a4d12cec9cf2cec68ef1c9d20ebc01
xinghen <154161795@qq.com>
2018-12-08 16:54:07 +0800

    vue

--- src/components/chart-center/chartConfigs/configs/styles/config-radar.vue
+++ src/components/chart-center/chartConfigs/configs/styles/config-radar.vue
@@ -1,3 +1,32 @@
 <template>
-    <div></div>
+ <div class="config-radar">
+      <div class="config-li">
+    <div class="config-title">填充设置</div>
+  </div>
+  <div class="config-li">
+    <div class="config-title">维度设置</div>
+    分几级，最大值
+  </div>
+   <div class="config-li">
+    <div class="config-title">线设置</div>
+    线宽、线类型、线颜色、分割线、轴线
+  </div>
+    <div class="config-li">
+    <div class="config-title">雷达图大小</div>
+  </div>
+</div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    config: {
+      required: true,
+      type: Object,
+      default: () => null
+    }
+  }
+};
+</script>
