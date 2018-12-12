9c47a8e74f081ef8a5388f3efea655757d9bcccf
xinghen <154161795@qq.com>
2018-12-12 22:32:01 +0800

    Update config-sunburst.vue

--- src/components/chart-center/chartConfigs/configs/styles/config-sunburst.vue
+++ src/components/chart-center/chartConfigs/configs/styles/config-sunburst.vue
@@ -1,43 +1,11 @@
 <template>
   <div class="config-pie">
     <div class="config-li">
-      <div class="config-title">饼图类型</div>
-      <div style="margin-left:14px;height:28px;line-height:28px">
-        <el-radio v-model="config.pieType" label="type1">类型1</el-radio>
-        <el-radio v-model="config.pieType" label="type2">类型2</el-radio>
-        <el-radio v-model="config.pieType" label="type3">类型3</el-radio>
-      </div>
     </div>
-     <div class="config-li">
-      <div class="config-title">饼图半径</div>
-       <div style="margin-left:14px;">
-     <div v-show="config.pieType!= 'type1'">内径：<el-input-number v-model="config.series.inRange" controls-position="right" :controls="false" size="medium"
-          style="width:180px;"></el-input-number>%</div>
-      <div v-show="config.pieType!= 'type1'">外径：<el-input-number v-model="config.series.outRange" controls-position="right" :controls="false" size="medium"
-          style="width:180px;"></el-input-number>%</div>
-    
-       <div v-show="config.pieType== 'type1'">半径：<el-input-number v-model="config.series.outRange" controls-position="right" :controls="false" size="medium"
-          ></el-input-number></div> 
-       </div>
-    </div>
-    <div class="config-li">
-      <div class="config-title">选中模式</div>
-      <div style="margin-left:16px;height:28px;line-height:28px">
-        <el-radio v-model="config.selectedMode" label="single">单选</el-radio>
-        <el-radio v-model="config.selectedMode" label="multiple">多选</el-radio>
-      </div>
-    </div>
-    <div class="config-li">
-      <div class="config-title">提示名称</div>
-      <el-input v-model="config.series.name" placeholder="请输入名称"></el-input>
-    </div>
-    <div class="config-li">
-      <div class="config-title">动画方式</div>
-      <div style="margin-left:16px;height:28px;line-height:28px">
-        <el-radio v-model="config.animation" label="expansion">展开</el-radio>
-        <el-radio v-model="config.animation" label="scale">缩放</el-radio>
-      </div>
-    </div>
+    <!-- name 
+    radius
+    highlightPolicy 
+    层级颜色levels -->
   </div>
 </template>
 <script>
@@ -53,7 +21,7 @@
       }
     },
     mounted() {
-      console.log(this.config);
+
     },
     methods: {}
   };
